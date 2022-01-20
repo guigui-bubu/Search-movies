e4a5fde300b635c796f129bfeb264d6d55497a6c
guigui-bubu <guillaumebuaud@yahoo.fr>
2022-01-20 16:02:54 +0100

    trier les films par étoiles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,16 @@ const Header = () => {
     <div className="header">
       <nav>
         <ul>
-          <NavLink to="/">
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
             <li>Accueil</li>
           </NavLink>
-          <NavLink to="/coup-de-coeur">
+          <NavLink
+            to="/coup-de-coeur"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
             <li>Coup de coeur</li>
           </NavLink>
         </ul>
